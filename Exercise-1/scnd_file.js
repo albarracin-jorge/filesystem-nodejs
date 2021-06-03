8c3e555735dc7f0d22eb61887fc677c81802ceae
= <=>
2021-06-03 13:38:35 -0300

    3rd commit

--- Exercise-1/scnd_file.js
+++ Exercise-1/scnd_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports = {
   createFile : ()=>{
     console.log("Creating text file...");
-    fs.appendFile("text.txt", "Hello World! Hola Mundo! ", (error)=>{
+    fs.appendFile("./Exercise-1/text.txt", "Hello World! Hola Mundo! ", (error)=>{
       if(error){
         console.log(`An error was happend creating file! ${error}`)
       }
@@ -12,7 +12,7 @@ module.exports = {
   },
   readFile : ()=>{
     console.log("Reading text file...");
-    fs.readFile("text.txt", 'utf-8', (error, data)=>{
+    fs.readFile("./Exercise-1/text.txt", 'utf-8', (error, data)=>{
       if(error){
         console.log(`An error was happend reading file! ${error}`)
       }
